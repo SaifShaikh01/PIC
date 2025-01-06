8fe79cb7257ac9c3cd904c3981f852164d30cb4b
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-06 19:58:48 +0530

    Hello Word
    ...

--- 01_HelloWord.c
+++ 01_HelloWord.c
@@ -1,5 +1,5 @@
 #include<stdio.h>                //Header file
-    int main()                   //main Function
+    int main()                   //main Function.
     {
         printf("Hello Word...");
         return 0;
