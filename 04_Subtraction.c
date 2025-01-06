f1209ae21195487dfcf1477266fe01cec1ddb2c6
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-06 20:14:07 +0530

    Create 04_Subtraction.c
    Subtraction code

---
+++ 04_Subtraction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main()
+    {
+        int num1,num2,sub;
+            printf("Enter a first number");
+            scanf("%d",&num1);
+
+            printf("Enter a second number");
+            scanf("%d",&num2);
+
+            sub=num1-num2;
+            
+        return 0;
+    }
