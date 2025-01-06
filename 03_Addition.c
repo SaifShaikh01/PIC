292a9f681cdf6141556cb8502349a00333419997
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-06 20:12:17 +0530

    Update 03_Addition.c

---
+++ 03_Addition.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main()
+    {
+        int num1,num2,sum;
+            printf("Enter a first number");
+            scanf("%d",&num1);
+
+            printf("Enter a second number");
+            scanf("%d",&num2);
+
+            sum=num1+num2;
+            
+        return 0;
+    }
