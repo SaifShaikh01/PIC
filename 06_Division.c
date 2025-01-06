38aedaca618c55fa080db22a43e6e88f6dc65e77
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-06 20:19:37 +0530

    Division Code

---
+++ 06_Division.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+    int main()
+    {
+        int num1,num2,div;
+            printf("Enter a first number");
+            scanf("%d",&num1);
+
+            printf("Enter a second number");
+            scanf("%d",&num2);
+
+            div=num1+num2;
+            
+        return 0;
+    }
