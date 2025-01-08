72d0467ad39babe7e8bed7b2eaf74008d6929aab
Saif Shaikh <shaikhsaif9500@gmail.com>
2025-01-08 22:49:17 +0530

    Character Print

--- 07_Print_character.c
+++ 07_Print_character.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char ch[20]; // Declare a character array with size 20
+
+    char ch[20]; 
+
     printf("Enter a string (up to 19 characters): ");
-    scanf("%19s", ch); // Use %19s to limit input to 19 characters + null terminator
-    printf("You entered: %s\n", ch);
-    return 0;
+    scanf("%s", ch);
+
+return 0;
 }
